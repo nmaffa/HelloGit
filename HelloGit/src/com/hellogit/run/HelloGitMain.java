24743a77cceee9b17f8bab2678e9da62868bf02f
nmaffa <nmaffattone@sapient.com>
2015-04-28 15:49:04 -0400

    Started HelloGit proj

---
+++ HelloGit/src/com/hellogit/run/HelloGitMain.java
@@ -0,0 +1,10 @@
+package com.hellogit.run;
+
+public class HelloGitMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!!");
+	}
+
+}
